703b02b4608dc4f526250b9107777be7e3e8951d
cli1903 <47994462+cli1903@users.noreply.github.com>
2019-11-25 00:45:02 -0500

    Update ConsolDis.py

--- ConsolDis.py
+++ ConsolDis.py
@@ -1,14 +1,8 @@
 '''
 Created on Jul 9, 2019
 
-@author: cindyli
 '''
 
-'''
-Created on Apr 29, 2019
-
-@author: cindyli
-'''
 from basicversion import getParents
 from quickumls import QuickUMLS
 import re
@@ -18,12 +12,12 @@ mpl.use('tkagg')
 import matplotlib.pyplot as plt
 #from matplotlib import cm
 
-matcher = QuickUMLS(quickumls_fp='/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/umls', overlapping_criteria="length", accepted_semtypes={"T047", "T191"})
-generalFile = '/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/basicversion/stats+info/diseases+count.txt'
-zeroFile = '/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/basicversion/stats+info/zero-zero.txt'
-oneFile = '/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/basicversion/stats+info/new-one-one-test1.txt'
-titleFile = '/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/basicversion/stats+info/titles.txt'
-graphStatsFile = '/Users/cindyli/Documents/CS/Research/workspace-biomed/pubmeddata/basicversion/stats+info/graph-degrees-weighted1.txt'
+matcher = QuickUMLS(quickumls_fp='umls', overlapping_criteria="length", accepted_semtypes={"T047", "T191"})
+generalFile = 'diseases+count.txt'
+zeroFile = 'zero-zero.txt'
+oneFile = 'one-one-test1.txt'
+titleFile = 'titles.txt'
+graphStatsFile = 'graph-degrees-weighted.txt'
 
 file = open(titleFile, 'r')
 
@@ -48,8 +42,12 @@ G = nx.DiGraph()
 counter = 0
 onesCounter = 0
 
-# given a title, gets the matches and stores them as two lists, before and after, and returns these two lists as a tuple
-# return (before, after), where before is the list of matches before the pattern and after is the list of matches after the pattern
+"""
+given a title, gets the matches and stores them as two lists, before and after, 
+and returns these two lists as a tuple
+return (before, after), where before is the list of matches before the pattern 
+and after is the list of matches after the pattern
+"""
 def handleMatches(title):
     m = matcher.match(title, best_match=True, ignore_syntax=False)
     terms = []
@@ -60,6 +58,7 @@ def handleMatches(title):
         sim = 0
         cui = 0
         term = ""
+    
         for posMatch in elem:
             if posMatch.get("end") - posMatch.get("start") > end - start:
                 start = posMatch.get("start")
@@ -98,9 +97,10 @@ def handleMatches(title):
         cuiToDis[after[0].get("cui")] = after[0].get("disease")
     return (before, after)
 
-
-# updates the counts of pairs
-def handleOneOne(befDis, aftDis):
+"""
+updates the counts of pairs of diseases
+"""
+def count_one_one(befDis, aftDis):
     if befDis in oneDict:
         if aftDis in oneDict[befDis]:
             oneDict[befDis][aftDis] += 1
@@ -109,7 +109,9 @@ def handleOneOne(befDis, aftDis):
     else:
         oneDict[befDis] = {aftDis: 1}
 
-# updates the counts of a disease being misdiagnosed            
+"""
+updates the counts of a disease being misdiagnosed    
+"""        
 def getNormal(befDis):
     if befDis in oneNormDict:
         oneNormDict[befDis] = oneNormDict[befDis] + 1
@@ -123,11 +125,13 @@ def getNormal(befDis):
     if both have SY, uses SY CUI
     if neither, check parents of both
 """           
-def handleParents(befDis, aftDis):
-    simDis1Par, simDis1Chd, simDis1Syn = getParents.parCUIList(befDis)
-    simDis2Par, simDis2Chd, simDis2Syn = getParents.parCUIList(aftDis)
+def handle_relations(befDis, aftDis):
+    simDis1Par, simDis1Chd, simDis1Syn = getParents.rel_cui_list(befDis)
+    simDis2Par, simDis2Chd, simDis2Syn = getParents.rel_cui_list(aftDis)
+    
     dis1 = befDis
     dis2 = aftDis
+    
     for simBef in simDis1Syn:
         if simBef != befDis and cuiToDis.get(simBef) != None:
             dis1 = simBef
@@ -135,25 +139,25 @@ def handleParents(befDis, aftDis):
         if simAft != aftDis and cuiToDis.get(simAft) != None:
             dis2 = simAft
     if dis1 != befDis and dis2 != aftDis:
-        print(befDis)
         getNormal(dis1)
-        handleOneOne(dis1, dis2)
+        count_one_one(dis1, dis2)
     elif dis1 != befDis:
-        print(befDis)
-        checkParents([dis1], simDis2Par, dis1, dis2, [dis1], simDis2Chd)
+        check_parents([dis1], simDis2Par, dis1, dis2, [dis1], simDis2Chd)
     elif dis2 != aftDis:
-        print(aftDis)
-        checkParents(simDis1Par, [dis2], dis1, dis2, simDis1Chd, [dis2])
+        check_parents(simDis1Par, [dis2], dis1, dis2, simDis1Chd, [dis2])
     else:
-        print("c4")
-        checkParents(simDis1Par, simDis2Par, befDis, aftDis, simDis1Chd, simDis2Chd)
+        check_parents(simDis1Par, simDis2Par, befDis, aftDis, simDis1Chd, simDis2Chd)
         
     
 """
     checks if parent CUIs are in dictionary and adds counts to parents
-    if children CUIs are in dictionary, adds children counts to it and removes children
+    if children CUIs are in dictionary, adds children counts to it and 
+    removes children
+    
+    parList1 and chdList1 correspond to befDis
+    parList2 and chdList2 correspond to aftDis
 """
-def checkParents(parList1, parList2, befDis, aftDis, chdList1, chdList2):
+def check_parents(parList1, parList2, befDis, aftDis, chdList1, chdList2):
     dis1 = befDis
     dis2 = aftDis
     if befDis in chdToPar:
@@ -175,11 +179,14 @@ def checkParents(parList1, parList2, befDis, aftDis, chdList1, chdList2):
     handleChildren(dis1, chdList1)
     handleChildren(dis2, chdList2)
     getNormal(dis1)
-    handleOneOne(dis1, dis2)
+    count_one_one(dis1, dis2)
     
     
 """
-    checks the children in chdList1 of befDis to see if it already exists and adds to parent if yes
+    checks the children in chdList1 of befDis to see if it already exists and 
+    adds to parent if yes
+    
+    for when befDis has a child but that child does not have befDis as a parent
 """                    
 def handleChildren(befDis, chdList1):
     for chd in chdList1:
@@ -205,7 +212,8 @@ def handleChildren(befDis, chdList1):
             
  
 """
-    writes to the zero-zero file if QuickUMLS extracts zero diseases on one side of the pattern
+    writes to the zero-zero file if QuickUMLS extracts zero diseases on one 
+    side of the pattern
     befList - list of disease names before the misdiagnosed/masquerading 
     aftList - list of diases names after the misdiagnosed/masquerading
     title - the title of the article
@@ -232,7 +240,7 @@ def addingCounts(dis1, dis2, disDict):
 """
     writes the title and the pairs to the general file while adding counts
 """    
-def handleOther(bef, aft, title, disDict):
+def handle_all(bef, aft, title, disDict):
     disFile.write(title + "\n")
     for dis1 in bef:
         for dis2 in aft:
@@ -266,8 +274,11 @@ def normalize():
                 G.add_edge(befDis, aftDis, weight = ratio)
                 counter = counter + 1
                 onesCounter = onesCounter + 1
-                #oneToone.write(" --> ".join((befDis, aftDis)) + "\n" + " --> ".join(pairTerms) + "\n    normalized count: " + ratio + "\n      total of first: " + 
-                #               str(oneNormDict[befDis]) +  "\n\n")
+                oneToone.write(" --> ".join((befDis, aftDis)) + "\n" + 
+                               " --> ".join(pairTerms) + 
+                               "\n    normalized count: " + ratio + 
+                               "\n      total of first: " + 
+                               str(oneNormDict[befDis]) +  "\n\n")
 
 """
     writes the counts of each disease pair from all titles in the general file
@@ -288,24 +299,26 @@ def writeTotNum():
         disFile.write(str(pair[0][0]) + "diseases --> " + str(pair[1][0]) + "\n")
 
 """
-    runs the methods
+    goes through each title, getting the matches, filters out the ones without 
+    exactly 1 cui on either side to get counts for
 """
 def runMatches():
     for line in file:
         matches = handleMatches(line.lower())
         getCounts(matches[0], matches[1], countsDict)
         if len(matches[0]) == 1 and len(matches[1]) == 1:
-            print("check0")
-            #handleOneOne(matches[0][0].get("cui"), matches[1][0].get("cui"))
-            handleParents(matches[0][0].get("cui"), matches[1][0].get("cui"))
-            print("check0.5")
+            count_one_one(matches[0][0].get("cui"), matches[1][0].get("cui"))
+            handle_relations(matches[0][0].get("cui"), matches[1][0].get("cui"))
         #elif len(matches[0]) == 0 or len(matches[1]) == 0:
          #   handleZeros(matches[0], matches[1], line)
-        #handleOther(matches[0], matches[1], line.lower(), nonOneDis)
+        #handle_all(matches[0], matches[1], line.lower(), nonOneDis)
 
 """
     to get a graph of related nodes 
-    (aka all the nodes connected to one disease and all the nodes connected to its connections, and so on)
+    (aka all the nodes connected to one disease and all the nodes connected to 
+    its connections, and so on)
+    
+    for visualization
 """     
 def getGroups(pair, graph):
     #group = {pair: oneDict.get(pair)}
@@ -329,14 +342,13 @@ def getGroups(pair, graph):
                 
 
 
-
-print("start")
 runMatches()
-print("check1")
 normalize()
-print("check3")
 #writeCounts()
 print("total pairs: " + str(counter) + ", ones pairs: " + str(onesCounter))
+
+
+""" graph visualization and degree analysis """
 
 pos=nx.spring_layout(G)
 fig = plt.figure(3,figsize=(12,12))
@@ -355,7 +367,8 @@ for v in d.values():
 for v in inDeg.values():
     totInDeg += v
 
-graphStats.write("avg out-degree-centrality: " + str(totOutDeg/numNodes) + "\n\navg in-degree-centrality: " + str(totInDeg/numNodes) + "\n\n")
+graphStats.write("avg out-degree-centrality: " + str(totOutDeg/numNodes) + 
+                 "\n\navg in-degree-centrality: " + str(totInDeg/numNodes) + "\n\n")
 
 totBet = nx.betweenness_centrality(G, k=numNodes)
 bet = 0
@@ -379,38 +392,13 @@ for n in list(G.nodes):
     totOut += G.out_degree(n)
     #graphStats.write(n +  "\nout-degree: " + str(nOut) + "\nin-degree: " + str(nIn) + "\nbetweeness centrality: " + str(totBet.get(n)) + "\n\n")
 
-graphStats.write("avg in-degree: " + str(totIn/numNodes) + "\navg out-degree: " + str(totOut/numNodes) + "\n\n")
+graphStats.write("avg in-degree: " + str(totIn/numNodes) + 
+                 "\navg out-degree: " + str(totOut/numNodes) + "\n\n")
 
+nx.draw(G,pos, edge_color=weights, width=1, node_color = 'b', with_labels=False, 
+        node_size=[v * 10000 + 10 for v in d.values()], font_size=8, 
+        edge_cmap=plt.cm.get_cmap('Purples'))
 
-"""topIn = sorted(inDeg.items(), key = lambda x : x[1])
-topOut = sorted(outDeg.items(), key = lambda x : x[1])
-
-top20In = topIn[-20:]
-top20Out = topOut[-20:]
-
-curVal = 0
-graphStats.write("Top In Degrees\n")
-for x in top20In:
-    curVal = x[1]
-    graphStats.write(x[0] + ": " + str(x[1]) + "\n     out-degree: " + str(outDeg.get(x[0])) + "\n\n")
-for x in topIn[:-20]:
-    if x[1] == curVal:
-        graphStats.write(x[0] + ": " + str(x[1]) + "\n     out-degree: " + str(outDeg.get(x[0])) + "\n\n")
-        
-        
-curVal = 0
-graphStats.write("Top Out Degrees\n")
-for x in top20Out:
-    curVal = x[1] 
-    graphStats.write(x[0] + ": " + str(x[1]) + "\n     in-degree: " + str(inDeg.get(x[0])) + "\n\n")
-for x in topOut[:-20]:
-    if x[1] == curVal:
-        graphStats.write(x[0] + ": " + str(x[1]) + "\n     in-degree: " + str(inDeg.get(x[0])) + "\n\n")"""
-        
-        
-        
-
-nx.draw(G,pos, edge_color=weights, width=1, node_color = 'b', with_labels=False, node_size=[v * 10000 + 10 for v in d.values()], font_size=8, edge_cmap=plt.cm.get_cmap('Purples'))
 fig.set_facecolor("skyblue")
 plt.show()
 
@@ -419,16 +407,3 @@ disFile.close()
 zeroTozero.close()
 oneToone.close()
 graphStats.close()
-
-
-            
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
